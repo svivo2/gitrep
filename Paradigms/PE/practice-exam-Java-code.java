bd614e930413d975a77237d7994a5023308a870f
svivo2 <vivo.sebastian@gmail.com>
2020-05-04 11:44:20 -0500

    Paradigms Practice Exam and Machine Learning update

---
+++ Paradigms/PE/practice-exam-Java-code.java
@@ -0,0 +1,5 @@
+/* [20 pts]
+Write the complete code for a java application which will print
+out the line "This is a practice exam" when run. Name your class
+"PracticeExam".
+*/
---
+++ Paradigms/PE/practice-exam-Python-code.py
@@ -0,0 +1,8 @@
+""" [20 pts]
+Define a Python function named "Evens" that takes one
+argument, which is assumed to be a list of integers,
+and returns a list containing all of the integers from
+the input list which are even numbers, in the same order.
+"""
+
+
